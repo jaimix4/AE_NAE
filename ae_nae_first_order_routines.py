corrected normalizations and plot to check numerical and analytical implementations

ae_nae_first_order_routines.py
@@ -75,11 +75,11 @@ class ae_nae_num:
         I = 0
         self.jac = (self.stel.G0 + self.iota*I)/self.Bmag**2
 
-        #dldvartheta
-        self.dldtheta = np.abs(self.modb/self.jac)
+        #dldvartheta !!!!!! I figure this out :))))
+        self.dldtheta = np.abs((self.modb*self.jac)/self.iotaN)
 
         # make lambda array
-        self.lam_arr = np.linspace(1/self.modb.max(),1/self.modb.min(),self.lam_res,endpoint=False)[1:]
+        self.lam_arr = np.linspace(1/self.modb.max(),1/self.modb.min(),self.lam_res)[1:-1]
 
         self.w_alpha = 'not computed yet'
         self.w_psi = 'not computed yet'
@@ -150,7 +150,7 @@ class ae_nae_num:
 
         Om = 1 # maybe this normalization is not doing well
 
-        Ghat = (tau_b*Om)/self.L
+        Ghat = tau_b/self.L
 
         #############################
         # DEFINE: calculate c0 and c1
@@ -173,6 +173,74 @@ class ae_nae_num:
         ae_total = simpson(ae_per_lam, self.lam_arr)
 
         return ae_per_lam, ae_total
+
+    def plot_ae_per_lam(self, dln_n_dpsi, dln_T_dpsi):
+
+        iotaN = self.iotaN
+
+        N = self.iota - self.iotaN
+
+        r = self.r
+
+        eta = self.eta
+
+        B0 = self.B0
+
+        Delta_psiA = self.Delta_psiA
+
+        lamb_arr = self.lam_arr
+
+        res = 5000
+
+        theta = np.linspace(-np.pi, np.pi, res)
+
+        phi = theta/(iotaN + N)
+
+        vartheta = theta - N*phi
+
+        magB = self.stel.B_mag(r=r, theta=theta, phi=phi)
+
+        vartheta_ae = 2*np.arcsin(np.sqrt((((1 - lamb_arr*(1 + r*eta))/(-r*eta*lamb_arr)))/2))
+
+        ae_per_lamb, ae_total = self.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)
+
+        ae_per_lamb_nor_total_ae =  ae_per_lamb/ae_total
+
+        norm = plt.Normalize()
+        colors = plt.cm.plasma(norm(ae_per_lamb_nor_total_ae))
+
+        cmap = plt.get_cmap('plasma', 200)
+        norm = mpl.colors.Normalize(vmin=np.min(ae_per_lamb_nor_total_ae), vmax=np.max(ae_per_lamb_nor_total_ae))
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array([])
+
+        fig,ax = plt.subplots(figsize=(11,5))
+
+        for idx, var_theta in enumerate(vartheta_ae):
+
+            B_vartheta = np.interp(var_theta, vartheta, magB)
+            ax.plot([-var_theta, var_theta], [B_vartheta, B_vartheta], c = colors[idx], linewidth = 0.9)
+
+        ax.set_title('Numerical, r = {} [m], $\eta$ = {}, B0 = {} [T], \n'.format(r, eta, B0) + r'$\Delta{\psi}_{A}$' + ' = {:.2f}'.format(Delta_psiA) + ', omT = {}, omn = {}'.format(dln_T_dpsi, dln_n_dpsi) + ', total AE = {:.2f}'.format(ae_total))
+        ax.plot(vartheta, magB, c = 'black')
+        fig.colorbar(sm, label = '$\hat{A}_{\lambda}/\hat{A}$', location = 'left', pad = 0.16)
+        ax.set_ylabel('$|B|$')
+        ax.set_xlabel(r'$\vartheta$')
+
+        ax2=ax.twinx()
+
+        ax2.plot(vartheta_ae, self.w_alpha, '-.r', linewidth = 0.9, alpha = 0.7)
+        ax2.plot(-vartheta_ae, self.w_alpha, '-.r', linewidth = 0.9, alpha = 0.7)
+        ax2.plot([-vartheta_ae[0] - 0.1, vartheta_ae[0]+ 0.1], [0,0], '--r', linewidth = 0.5)
+
+        ax2.set_ylabel(r'$\hat{\omega}_{\alpha}$', color = 'r')
+        ax2.spines['right'].set_color('r')
+        ax2.tick_params(colors='red', which='major')
+
+        plt.show()
+
+
+
 
 
 class ae_nae:
@@ -203,7 +271,7 @@ class ae_nae:
 
         self.lamb_max = lamb_max = 1/(1 + r*self.eta)
 
-        self.lam_arr = np.linspace(self.lamb_min, self.lamb_max, lam_res)
+        self.lam_arr = np.linspace(self.lamb_min, self.lamb_max, lam_res)[1:-1]
 
         self.ellip_n = ((-1*(1 - (1 + r*self.eta)*self.lam_arr))/((1 + r*self.eta)*self.lam_arr))
 
@@ -265,7 +333,7 @@ class ae_nae:
 
         den_dJnor_dpsi = r*(-1 + r*eta)* (B0 + B0*r*eta)**2 * np.sqrt(-1 * r * eta * lamb)
 
-        dJnor_dpsi = -num_dJnor_dpsi/den_dJnor_dpsi
+        dJnor_dpsi = -2*num_dJnor_dpsi/den_dJnor_dpsi
 
         #############################
         # DEFINE: the normalized w_alpha (ralf normalization for AE, taking into account jaime normalization)
@@ -299,13 +367,15 @@ class ae_nae:
 
         Om = 1 # maybe this normalization is not doing well
 
-        Ghat = (tau_nor*Om)/self.L
+        Ghat = tau_nor/self.L
 
         #############################
         # DEFINE: calculate c0 and c1
 
-        c0 = self.Delta_psiA * (dln_n_dpsi - 3/2 * dln_T_dpsi) / w_alpha_nor
-        c1 = 1.0 - self.Delta_psiA * dln_T_dpsi / w_alpha_nor
+        # here is where you apply the minus signs of the w_alpha_nor?
+
+        c0 = (self.Delta_psiA * (dln_n_dpsi - 3/2 * dln_T_dpsi)) / w_alpha_nor
+        c1 = 1.0 - (self.Delta_psiA * dln_T_dpsi) / w_alpha_nor
 
         condition1 = np.logical_and((c0>=0),(c1<=0))
         condition2 = np.logical_and((c0>=0),(c1>0))
@@ -371,7 +441,7 @@ class ae_nae:
             B_vartheta = np.interp(var_theta, vartheta, magB)
             ax.plot([-var_theta, var_theta], [B_vartheta, B_vartheta], c = colors[idx], linewidth = 0.9)
 
-        ax.set_title('r = {} [m], $\eta$ = {}, B0 = {} [T], \n'.format(r, eta, B0) + r'$\Delta{\psi}_{A}$' + ' = {:.2f}'.format(Delta_psiA) + ', omT = {}, omn = {}'.format(dln_T_dpsi, dln_n_dpsi) + ', total AE = {:.2f}'.format(ae_total))
+        ax.set_title('Analytical, r = {} [m], $\eta$ = {}, B0 = {} [T], \n'.format(r, eta, B0) + r'$\Delta{\psi}_{A}$' + ' = {:.2f}'.format(Delta_psiA) + ', omT = {}, omn = {}'.format(dln_T_dpsi, dln_n_dpsi) + ', total AE = {:.2f}'.format(ae_total))
         ax.plot(vartheta, magB, c = 'black')
         fig.colorbar(sm, label = '$\hat{A}_{\lambda}/\hat{A}$', location = 'left', pad = 0.16)
         ax.set_ylabel('$|B|$')

tests/simple_test.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/Users/jaimecaballero/Desktop/TUe_thesis/code/AEpy-main/AE_NAE_py')
 from ae_nae_first_order_routines import  *
 from qsc import Qsc
+import matplotlib.pyplot as plt
 
 # A configuration is defined with B0, etabar, field periods, Fourier components and eta bar
 
@@ -22,33 +23,181 @@ zs=[0, -0.045]  # zs -> the sine components of the axis vertical component
 stel = Qsc(rc=[1, 0.045], zs=[0, -0.045], nfp=nfp, etabar=eta, B0=B0)
 
 # to calculate things the lam_res needs to be provided, just that?
-lam_res = 10
+lam_res = 1000
 
+# distance from the magnetic axis to be used
 r = 0.05
 
+# normalization variable for AE
 Delta_psiA = 1
 
-dln_n_dpsi = -10
-
+# gradients for diamagnetic frequency
+dln_n_dpsi = 5
 dln_T_dpsi = 0
 
+#declaring the object to compute ae using anlaytical form
+
 stel_ae = ae_nae(stel, r, lam_res, Delta_psiA)
+
+#declaring the object to compute ae using numerical form
 
 vartheta_res = 1000
 
 stel_ae_num = ae_nae_num(stel, r, lam_res, vartheta_res, Delta_psiA)
 
-print('analytical')
-print(stel_ae.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)[1])
 
-print('numerical')
-print(stel_ae_num.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)[1])
+# 1st plot comparison of bouncing times and precession frequencies
+##################################################################
 
-# print(stel_ae.bounce_time_nor_analytical())
-#
+tau_b_num, w_alpha_num, w_psi_num = stel_ae_num.tau_b_w_alpha_w_psi()
+
+lamb_arr_num = stel_ae_num.lam_arr
+
+tau_b_anal = stel_ae.bounce_time_nor_analytical()
+w_alpha_anal = stel_ae.w_alpha_nor_analytical()
+lamb_arr_anal = stel_ae.lam_arr
 
 
-#
-# print(stel_ae.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi))
 
-# stel_ae.plot_ae_per_lam(dln_n_dpsi, dln_T_dpsi)
+fig,ax = plt.subplots(figsize=(11,5))
+ax.grid(alpha = 0.5)
+
+ax.plot(lamb_arr_anal, tau_b_anal, 'c', label = r'$\hat{\tau}_{b}$' + ' analytical')
+ax.plot(lamb_arr_num, tau_b_num, '-.k', label = r'$\hat{\tau}_{b}$' + ' numerical')
+ax.set_ylabel(r'$\hat{\tau}_{b}$')
+ax.set_xlabel('$\lambda$')
+
+ax.legend(loc = 3)
+
+ax2=ax.twinx()
+
+ax2.plot(lamb_arr_anal, w_alpha_anal, label = r'$\hat{\omega}_{\alpha}$' + ' analytical')
+ax2.plot(lamb_arr_num, w_alpha_num, '-.', label = r'$\hat{\omega}_{\alpha}$' + ' numerical')
+
+ax2.plot(lamb_arr_anal, 0*w_alpha_anal, label = r'$\hat{\omega}_{\psi}$' + ' analytical')
+ax2.plot(lamb_arr_num, w_psi_num, '-.', label = r'$\hat{\omega}_{\psi}$' + ' numerical')
+
+ax2.set_ylabel(r'$\hat{\omega}_{\psi}$' + ', ' + r'$\hat{\omega}_{\alpha}$')
+ax2.legend(loc = 1)
+ax2.set_title('Standard QA configuration \n' + r'$\vartheta_{res.}$' + '(for numerical) = {},   '.format(vartheta_res) + r'$\lambda_{res.}$' + '(for both) = {},  r = {}'.format(lam_res, r))
+
+plt.show()
+
+
+# 2nd plot, different total available energy results comparison for
+# dln_n_dpsi
+##################################################################
+
+# array with different gradients
+#omn_arr = np.linspace(-10, 10, 5)
+
+omn_arr = [-10, -1, 1, 10]
+
+fig,ax = plt.subplots(figsize=(11,5))
+
+ax.set_xlabel('$\lambda$')
+ax.set_ylabel('$\hat{A}_{\lambda}/\hat{A}$')
+ax.grid(alpha = 0.5)
+ax.set_title('Standard QA configuration \n' + r'$\vartheta_{res.}$' + '(for numerical) = {},   '.format(vartheta_res) + r'$\lambda_{res.}$' + '(for both) = {},  r = {}'.format(lam_res, r))
+
+for idx, dln_n_dpsi in enumerate(omn_arr):
+
+    ae_per_lam, ae_total = stel_ae.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)
+    ae_per_lam_total = ae_per_lam/ae_total
+
+    ax.plot(lamb_arr_anal, ae_per_lam_total, label = 'omn (anlaytical) = {:.2f}, AE = {:.4f}'.format(dln_n_dpsi, ae_total))
+
+
+    ae_per_lam_num, ae_total_num = stel_ae_num.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)
+    ae_per_lam_total_num = ae_per_lam_num/ae_total_num
+
+    ax.plot(lamb_arr_num, ae_per_lam_total_num, '-.', label = 'omn (numerical) = {:.2f}, AE = {:.4f}'.format(dln_n_dpsi, ae_total_num))
+
+ax.legend()
+ax.set_title('r = {}, eta = {}, omT = {}'.format(r, eta, dln_T_dpsi))
+
+ax2=ax.twinx()
+
+ax2.plot(lamb_arr_anal, w_alpha_anal, '-.k')
+ax2.plot(lamb_arr_anal, 0*w_alpha_anal, '--k')
+ax2.set_ylabel(r'$\hat{\omega}_{\alpha}$')
+
+plt.show()
+
+
+# 3rd plot, for an specific omn, graph the difference in the ralf style plot
+############################################################################
+
+# gradients for diamagnetic frequency
+dln_n_dpsi = -5
+dln_T_dpsi = 0
+
+
+# setting vectors of B for plotting
+
+iotaN = stel.iotaN
+
+N = stel.iota - stel.iotaN
+
+res = 5000
+
+theta = np.linspace(-np.pi, np.pi, res)
+
+phi = theta/(iotaN + N)
+
+vartheta = theta - N*phi
+
+magB = stel.B_mag(r=r, theta=theta, phi=phi)
+
+vartheta_ae = 2*np.arcsin(np.sqrt((((1 - lamb_arr_anal*(1 + r*eta))/(-r*eta*lamb_arr_anal)))/2))
+
+# calculating ae
+
+ae_per_lam, ae_total = stel_ae.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)
+ae_per_lam_num, ae_total_num = stel_ae_num.ae_integrand_per_lamb_total(dln_n_dpsi, dln_T_dpsi)
+
+# error
+error_arr_plots = np.abs((ae_per_lam/ae_total) - (ae_per_lam_num/ae_total_num))
+
+norm = plt.Normalize()
+colors = plt.cm.plasma(norm(error_arr_plots))
+
+
+cmap = plt.get_cmap('plasma', 200)
+norm = mpl.colors.Normalize(vmin=np.min(error_arr_plots), vmax=np.max(error_arr_plots))
+sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+sm.set_array([])
+
+
+fig,ax = plt.subplots(figsize=(11,7))
+
+
+for idx, var_theta in enumerate(vartheta_ae):
+
+    B_vartheta = np.interp(var_theta, vartheta, magB)
+    ax.plot([-var_theta, var_theta], [B_vartheta, B_vartheta], c = colors[idx], linewidth = 1.1)
+
+ax.set_title('r = {} [m], $\eta$ = {}, B0 = {} [T], \n'.format(r, eta, B0) + r'$\Delta{\psi}_{A}$' + ' = {:.2f}'.format(1) + ', omT = {}, omn = {}'.format(dln_T_dpsi, dln_n_dpsi) + '\n AE analytical: {},  AE numerical: {} \n'.format(ae_total, ae_total_num) + r'$\vartheta_{res.}$' + '(for numerical) = {},   '.format(vartheta_res) + r'$\lambda_{res.}$' + '(for both) = {},  r = {}'.format(lam_res, r))
+ax.plot(vartheta, magB, c = 'black')
+fig.colorbar(sm, label = r'|$\hat{A}_{\lambda}/\hat{A}_{analytical}$ - $\hat{A}_{\lambda}/\hat{A}_{numerical}$|', location = 'left', pad = 0.16)
+ax.set_ylabel('$|B|$')
+ax.set_xlabel(r'$\vartheta$')
+
+ax2=ax.twinx()
+
+ax2.plot(vartheta_ae, w_alpha_anal, '-.k', linewidth = 0.9, alpha = 0.7)
+ax2.plot(-vartheta_ae, w_alpha_anal, '-.k', linewidth = 0.9, alpha = 0.7)
+ax2.plot([-vartheta_ae[0] - 0.1, vartheta_ae[0]+ 0.1], [0,0], '--k', linewidth = 0.5)
+ax2.set_ylabel(r'$\hat{\omega}_{\alpha}$')
+
+plt.show()
+
+
+###################################################################################
+
+
+stel_ae_num.plot_ae_per_lam(dln_n_dpsi, dln_T_dpsi)
+
+
+
+stel_ae.plot_ae_per_lam(dln_n_dpsi, dln_T_dpsi)